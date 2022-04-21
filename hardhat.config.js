956c03d8a189148cf591fd7ca01a5367235e90d4
kingzamzon <adesanoye.samson@yahoo.com>
2022-04-21 03:51:16 +0100

    preparing smart contract for testing

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config();
+const fs = require("fs");
 require("@nomiclabs/hardhat-ethers");
+//require solidity-coverage
+require('solidity-coverage');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -21,14 +24,37 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
+function mnemonic() {
+   try {
+     return fs.readFileSync("./mnemonic.txt").toString().trim();
+   } catch (e) {
+     if (defaultNetwork !== "localhost") {
+       console.log(
+         "☢️ WARNING: No mnemonic file created for a deploy account. Try `yarn run generate` and then `yarn run account`."
+       );
+     }
+   }
+   return "";
+ }
+
 module.exports = {
    solidity: "0.8.5",
-   defaultNetwork: "rinkeby",
+   defaultNetwork: "localhost",
    networks: {
-      hardhat: {},
+      localhost: {
+         url: "http://localhost:8545",
+         // notice no mnemonic here? it will just use account 0 of the hardhat node to deploy
+         // (you can put in a mnemonic here to set the deployer locally)
+       },
       rinkeby: {
-         url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
+         url: "https://rinkeby.infura.io/v3/ec6a8acd1d354717acec099ad46a0bab", // <---- YOUR INFURA ID! (or it won't work)
+         accounts: {
+            mnemonic: mnemonic(),
+         },
       }
    },
+   etherscan: {
+      // your api key here.
+      apiKey: "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8",
+    },
 }
